620d5c7ab4ebb51386b7affda2de44423824137f
Kiarash Zarinmehr <kiarash.zar@gmail.com>
2018-07-08 00:10:26 +0430

    Initial commit

---
+++ src/routes/game/Game.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+import style from './style';
+
+const Game = () => (
+	<div class={style.home}>
+		<h1>Game</h1>
+		<p>This is the Game component.</p>
+	</div>
+);
+
+export default Game;
