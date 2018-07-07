d65c44eb2febf833906984784b24ef92e0b38f7d
Kiarash Zarinmehr <kiarash.zar@gmail.com>
2018-07-08 01:01:32 +0430

    Fix some indents

--- src/components/App.js
+++ src/components/App.js
@@ -6,24 +6,23 @@ import Game from '../routes/game';
 // import Profile from 'async!../routes/profile';
 
 class App extends Component {
+  /** Gets fired when the route changes.
+   *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+   *	@param {string} event.url	The newly routed URL
+   */
+  handleRoute = e => {
+    this.currentUrl = e.url;
+  };
 
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
-
-	render() {
-		return (
-			<div id="app">
-				<Router onChange={this.handleRoute}>
-					<Game path="/game" />
-				</Router>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="app">
+        <Router onChange={this.handleRoute}>
+          <Game path="/game" />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
