358f5142bbadce3a43c0a6716979ba1c4900a2f8
Hossein shabani <hosseinshabani.work@gmail.com>
2018-07-20 00:40:44 +0430

    add first animated backgroudn motions

--- src/components/AnimatedBackground.js
+++ src/components/AnimatedBackground.js
@@ -6,25 +6,31 @@ class AnimatedBackground extends Component {
   svg = {
     lineOne: {
       offset: 700,
-      width: '1em',
+      width: '0.9em',
       color: 'rgba(101,45,212,0.3)'
     },
     lineTwo: {
       offset: 1300,
       width: '0.8em',
-      color: '#FFBD40'
+      color: 'rgba(255, 162, 38,0.3)'
+    },
+    lineThree: {
+      offset: 2300,
+      width: '0.7em',
+      color: 'rgba(101,45,212,0.3)'
     }
   };
 
   componentDidMount = () => {
-    const { lineOne, lineTwo } = this.svg;
+    const { lineOne, lineTwo, lineThree } = this.svg;
 
-    this.TLcontrols = anime.timeline({
+    this.drawLineTL = anime.timeline({
       loop: true
     });
+    this.bubblesMotions = [];
 
-    this.TLcontrols.add({
-      targets: '#lineOne path',
+    this.drawLineTL.add({
+      targets: '#bubbles .bubbles__one',
       strokeDashoffset: [
         { value: [lineOne.offset, 0] },
         { value: [0, -lineOne.offset] }
@@ -33,9 +39,8 @@ class AnimatedBackground extends Component {
       offset: 300,
       easing: 'easeInOutSine'
     });
-
-    this.TLcontrols.add({
-      targets: '#lineTwo path',
+    this.drawLineTL.add({
+      targets: '#bubbles .bubbles__two',
       strokeDashoffset: [
         { value: [lineTwo.offset, 0] },
         { value: [0, -lineTwo.offset] }
@@ -44,52 +49,118 @@ class AnimatedBackground extends Component {
       offset: 1100,
       easing: 'easeInOutQuart'
     });
+    this.drawLineTL.add({
+      targets: '#bubbles .bubbles__three',
+      strokeDashoffset: [
+        { value: [lineThree.offset, 0] },
+        { value: [0, -lineThree.offset] }
+      ],
+      duration: 2800,
+      offset: 2100,
+      easing: 'easeInOutQuart'
+    });
 
-    this.TLcontrols.play();
+    var path_one = anime.path('#bubbles .bubbles__one');
+    this.bubblesMotions[0] = anime({
+      targets: '.bubbles .bubble_1',
+      translateX: path_one('x'),
+      translateY: path_one('y'),
+      easing: 'easeInQuad',
+      duration: 15000,
+      loop: true
+    });
+
+    var path_two = anime.path('#bubbles .bubbles__two');
+    this.bubblesMotions[1] = anime({
+      targets: '.bubbles .bubble_2',
+      translateX: path_two('x'),
+      translateY: path_two('y'),
+      easing: 'linear',
+      duration: 25000,
+      loop: true
+    });
+
+    var path_two = anime.path('#bubbles .bubbles__three');
+    this.bubblesMotions[2] = anime({
+      targets: '.bubbles .bubble_3',
+      translateX: path_two('x'),
+      translateY: path_two('y'),
+      easing: 'linear',
+      duration: 15000,
+      loop: true
+    });
+
+    this.drawLineTL.play();
+    // this.bubblesTL.play();
   };
 
   render() {
     const { children } = this.props;
-    const { lineOne, lineTwo } = this.svg;
+    const { lineOne, lineTwo, lineThree } = this.svg;
 
     return (
       <div class={styles.animatedBg__wrapper}>
         <div class={styles.animatedBg__animateArea}>
           <svg
-            id="lineOne"
-            class={styles.animatedBg__animateArea_lineOne}
+            id="bubbles"
+            class={styles.animatedBg__animateArea_bubbles}
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 629.882 122.054"
-            preserveAspectRatio="none"
+            viewBox="0 0 300 500"
+            preserveAspectRatio="xMidYMid slice"
           >
-            <g fill="none" class="lines" fill-rule="evenodd">
+            <g fill="none" class="roads">
               <path
                 stroke={lineOne.color}
-                d=" M 632.8 109.539 Q 493.64 10.947 403.882 15.029 C 324.662 18.632 283.501 105.829 203.882 107.029 Q 122.769 108.252 -1.685 17.976"
                 style={{
                   strokeDashoffset: lineOne.offset,
                   strokeDasharray: lineOne.offset,
                   strokeWidth: lineOne.width
                 }}
+                className="bubbles__one"
+                d=" M 224.092 504.5 Q 174.362 364.976 181.76 286.084 C 187.637 223.413 207.993 172.754 237.61 117.469 Q 257.332 80.653 305.198 20"
               />
-            </g>
-          </svg>
-          <svg
-            id="lineTwo"
-            class={styles.animatedBg__animateArea_lineTwo}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 744.172 191.29"
-            preserveAspectRatio="none"
-          >
-            <g fill="none" class="lines" fill-rule="evenodd">
               <path
                 stroke={lineTwo.color}
-                d=" M 753.066 47.584 Q 529.625 234.584 407.929 155.685 C 379.237 137.083 345.191 122.323 339.398 87.566 C 333.605 52.809 367.399 15.277 406.274 15.002 C 443.448 14.74 474.288 35.431 474.288 81.774 C 474.288 128.116 445.841 149.325 386.413 168.926 C 326.985 188.527 266.257 105.169 193.595 100.24 Q 114.006 94.841 -23.999 151.855"
                 style={{
                   strokeDashoffset: lineTwo.offset,
                   strokeDasharray: lineTwo.offset,
                   strokeWidth: lineTwo.width
                 }}
+                className="bubbles__two"
+                d=" M 138 502 Q 184.673 437.572 181 396 C 177.657 358.156 131.393 343.061 129 305 C 126.498 265.195 168.247 243.647 173 205 C 180.129 147.029 106.523 109.717 123 52 Q 128.958 31.131 152 -30"
+              />
+              <path
+                stroke={lineThree.color}
+                style={{
+                  strokeDashoffset: lineThree.offset,
+                  strokeDasharray: lineThree.offset,
+                  strokeWidth: lineThree.width
+                }}
+                className="bubbles__three"
+                d=" M 6.38 518 Q 64.34 440.291 67.38 389 C 69.37 355.426 48.303 329.544 49.38 296 C 50.764 252.877 94.121 224.314 87.38 181 C 80.639 137.686 -22.088 81.897 -24 -32"
+              />
+            </g>
+            <g class="bubbles">
+              <circle
+                className="bubble_1"
+                cx="0"
+                cy="0"
+                r="13"
+                fill={lineOne.color}
+              />
+              <circle
+                className="bubble_2"
+                cx="0"
+                cy="0"
+                r="15"
+                fill={lineTwo.color}
+              />
+              <circle
+                className="bubble_3"
+                cx="0"
+                cy="0"
+                r="15"
+                fill={lineOne.color}
               />
             </g>
           </svg>
